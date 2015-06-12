e7cd589234b331a96a89dd23900885d63d26e431
Abzal Serekov <abzal.serekov@gmail.com>
2015-06-12 04:55:38 -0500

    corrected

--- upload.php
+++ upload.php
@@ -12,6 +12,7 @@ $final_message .= "\ntext\n: ".$text;
 
 mail("aserekov@gmail.com", "New order from ".$contact_name, $final_message, "From:web@atta.kz" );
 mail("abzal.serekov@gmail.com", "New order from ".$contact_name, $final_message, "From:web@atta.kz" );
+mail($email, "Atta Studios: Ваше сообщение дощло успешно", "Здравствуйте ".$contact_name."\n\nСпасибо, что нам написали!\nМы с вами свяжемся в очень короткие сроки\n\nAtta Studios,\nAlmaty, Kazakhstan\n2015", "From:web@atta.kz" );
 
 header('Location: /index.php');    
 
